b9dc49259f6e2f5e449908904d238411ee9b546c
Erica <ehyleong@gmail.com>
2017-09-08 03:46:08 +0000

    Added RESTful logic, cleaned up ERBs and made minor styling changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,14 @@
 class Post < ActiveRecord::Base
     
+    #these are associations
     belongs_to :user
     has_many :comments
     has_many :likes
     
+    #validations in between association definitions and methods
+    validates_presence_of :photo_url, :user
+    
+    #logic for displaying time in a readable manner
     def humanized_time_ago
         time_ago_in_seconds = Time.now - self.created_at
         time_ago_in_minutes = time_ago_in_seconds / 60
@@ -15,10 +20,12 @@ class Post < ActiveRecord::Base
         end
     end
     
+    #logic for displaying like count as equal to post's number of likes
     def like_count
         self.likes.size
     end
     
+    #logic for displaying comment count as equal to post's number of likes
     def comment_count
         self.comments.size
     end
